8a3ed7fa188ba17981947bf7f074ce719f1f36fe
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-03-25 15:52:51 -0500

    Move a couple of methods into a dedicated TestUtils class
    Incidentally, this fixes a problem where the temporary directories were
    not created in the target/ directory.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src/test/java/imagej/build/minimaven/BasicTest.java
+++ src/test/java/imagej/build/minimaven/BasicTest.java
@@ -73,18 +73,18 @@ public class BasicTest {
 
 		final File blub = new File(tmp, "target/blub-1.0.0.jar");
 		assertTrue(blub.exists());
-		assertEquals("1.0.0\n", read(new JarFile(blub), "version.txt"));
+		assertEquals("1.0.0\n", TestUtils.read(new JarFile(blub), "version.txt"));
 		FileUtils.deleteRecursively(tmp);
 	}
 
 	@Test
 	public void testCopyToImageJApp() throws Exception {
 		final File tmp = writeExampleProject();
-		final File ijDir = createTemporaryDirectory("ImageJ.app-");
+		final File ijDir = TestUtils.createTemporaryDirectory("ImageJ.app-");
 		final File jarsDir = new File(ijDir, "jars");
 		assertTrue(jarsDir.mkdir());
 		final File oldVersion = new File(jarsDir, "blub-0.0.5.jar");
-		writeFile(oldVersion, "old");
+		TestUtils.writeFile(oldVersion, "old");
 		assertTrue(oldVersion.exists());
 
 		final BuildEnvironment env = new BuildEnvironment(null, false,
@@ -96,14 +96,14 @@ public class BasicTest {
 		assertTrue(blub.exists());
 		assertFalse(oldVersion.exists());
 
-		FileUtils.deleteRecursively(tmp);
+		FileUtils.deleteRecursively(project.directory);
 		FileUtils.deleteRecursively(ijDir);
 	}
 
 	@Test
 	public void testExcludeDependencies() throws Exception {
 		final BuildEnvironment env = new BuildEnvironment(null, false, false, false);
-		final File directory = createTemporaryDirectory("excludes-test-");
+		final File directory = TestUtils.createTemporaryDirectory("excludes-test-");
 
 		final String pom = pomPrefix
 				+ "\t<groupId>test2</groupId>\n"
@@ -176,123 +176,13 @@ public class BasicTest {
 
 	private File writeExampleProject() throws IOException {
 		final File tmp = createTemporaryDirectory("minimaven-");
-		writeFile(new File(tmp, "src/main/resources/version.txt"),
+		TestUtils.writeFile(new File(tmp, "src/main/resources/version.txt"),
 				"1.0.0\n");
-		writeFile(
+		TestUtils.writeFile(
 				new File(tmp, "pom.xml"), pomPrefix
 						+ "\t<groupId>test</groupId>\n"
 						+ "\t<artifactId>blub</artifactId>\n"
 						+ "\t<version>1.0.0</version>\n" + "</project>");
 		return tmp;
 	}
-
-	private static void writeFile(final File file, final String contents)
-			throws IOException {
-		final File dir = file.getParentFile();
-		if (dir != null && !dir.isDirectory() && !dir.mkdirs())
-			throw new IOException("Could not make " + dir);
-		final FileWriter writer = new FileWriter(file);
-		writer.write(contents);
-		writer.close();
-	}
-
-	private static String read(final JarFile jar, final String path)
-			throws IOException {
-		final ZipEntry entry = jar.getEntry(path);
-		final InputStream in = jar.getInputStream(entry);
-		final BufferedReader reader = new BufferedReader(new InputStreamReader(
-				in));
-		final StringBuilder builder = new StringBuilder();
-		for (;;) {
-			final String line = reader.readLine();
-			if (line == null)
-				break;
-			builder.append(line).append("\n");
-		}
-		reader.close();
-		return builder.toString();
-	}
-
-	/**
-	 * Makes a temporary directory for use with unit tests.
-	 * <p>
-	 * When the unit test runs in a Maven context, the temporary directory will be
-	 * created in the <i>target/</i> directory corresponding to the calling class
-	 * instead of <i>/tmp/</i>.
-	 * </p>
-	 * 
-	 * @param prefix the prefix for the directory's name
-	 * @return the reference to the newly-created temporary directory
-	 * @throws IOException
-	 */
-	private static File createTemporaryDirectory(final String prefix) throws IOException {
-		return createTemporaryDirectory(prefix, getCallingClass(null));
-	}
-
-	/**
-	 * Makes a temporary directory for use with unit tests.
-	 * <p>
-	 * When the unit test runs in a Maven context, the temporary directory will be
-	 * created in the corresponding <i>target/</i> directory instead of
-	 * <i>/tmp/</i>.
-	 * </p>
-	 * 
-	 * @param prefix the prefix for the directory's name
-	 * @param forClass the class for context (to determine whether there's a
-	 *          <i>target/<i> directory)
-	 * @return the reference to the newly-created temporary directory
-	 * @throws IOException
-	 */
-	private static File createTemporaryDirectory(final String prefix,
-		final Class<?> forClass) throws IOException
-	{
-		final URL directory = ClassUtils.getLocation(forClass);
-		if (directory != null && "file".equals(directory.getProtocol())) {
-			final String path = directory.getPath();
-			if (path != null && path.endsWith("/target/test-classes/")) {
-				final File baseDirectory =
-					new File(path.substring(0, path.length() - 13));
-				final File file = File.createTempFile(prefix, "", baseDirectory);
-				if (file.delete() && file.mkdir()) return file;
-			}
-		}
-		return FileUtils.createTemporaryDirectory(prefix, "");
-	}
-
-	/**
-	 * Returns the class of the caller (excluding the specified class).
-	 * <p>
-	 * Sometimes it is convenient to determine the caller's context, e.g. to
-	 * determine whether running in a maven-surefire-plugin context (in which case
-	 * the location of the caller's class would end in
-	 * <i>target/test-classes/</i>).
-	 * </p>
-	 * 
-	 * @param excluding the class to exclude (or null)
-	 * @return the class of the caller
-	 */
-	public static Class<?> getCallingClass(final Class<?> excluding) {
-		final String thisClassName = BasicTest.class.getName();
-		final String thisClassName2 = excluding == null ? null : excluding.getName();
-		final Thread currentThread = Thread.currentThread();
-		for (final StackTraceElement element : currentThread.getStackTrace()) {
-			final String thatClassName = element.getClassName();
-			if (thatClassName == null || thatClassName.equals(thisClassName) ||
-				thatClassName.equals(thisClassName2) ||
-				thatClassName.startsWith("java.lang.")) {
-				continue;
-			}
-			final ClassLoader loader = currentThread.getContextClassLoader();
-			try {
-				return loader.loadClass(element.getClassName());
-			}
-			catch (ClassNotFoundException e) {
-				throw new UnsupportedOperationException("Could not load " +
-					element.getClassName() + " with the current context class loader (" +
-					loader + ")!");
-			}
-		}
-		throw new UnsupportedOperationException("No calling class outside " + thisClassName + " found!");
-	}
-
 }
