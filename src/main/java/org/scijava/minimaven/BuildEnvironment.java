9a991078977fd021f044542d207fedd62ae21ebe
Curtis Rueden <ctrueden@wisc.edu>
2014-04-21 21:08:21 -0500

    Update licenses
    This was done using:
       mvn license:update-file-header
       mvn license:update-project-license

--- src/main/java/org/scijava/minimaven/BuildEnvironment.java
+++ src/main/java/org/scijava/minimaven/BuildEnvironment.java
@@ -1,10 +1,9 @@
 /*
  * #%L
- * ImageJ software for multidimensional image processing and analysis.
+ * MiniMaven build system for small Java projects.
  * %%
- * Copyright (C) 2009 - 2014 Board of Regents of the University of
- * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
- * Institute of Molecular Cell Biology and Genetics.
+ * Copyright (C) 2012 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
